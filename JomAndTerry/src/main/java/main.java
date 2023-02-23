cc4dfcc108cb4b25d14ebbdb544b44475bb850d1
wendix <wendi_xiao@sfu.ca>
2023-02-23 13:29:14 -0800

    fixed maven file

--- JomAndTerry/src/main/java/main.java
+++ JomAndTerry/src/main/java/main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("test");
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Jom and Terry");
+
+        // create gamePanel class
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
