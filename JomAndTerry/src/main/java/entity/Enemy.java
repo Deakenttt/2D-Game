dd770c8f50fe05f5dfc0563ccd145f337a331936
kcl35 <kcl35@sfu.ca>
2023-02-25 16:15:55 -0800

    forgot to add these files and made cat movement more natural

---
+++ JomAndTerry/src/main/java/entity/Enemy.java
@@ -0,0 +1,61 @@
+package entity;
+
+import main.GamePanel;
+import utility.KeyHandler;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+public class Enemy extends Entity{
+    
+    public Enemy(GamePanel gp, Color setColour){
+        super(gp);
+        direction = "down";
+        speed = 1;        
+        colour = setColour;
+    }
+
+    public void draw(Graphics2D g2) {
+        System.out.println("Setting Colour");
+
+        g2.setColor(colour);
+        g2.drawRect(x, y, 48, 48);
+    }
+    
+    public void setAction() {
+        actionLockC++;
+        
+        if (actionLockC == 90){
+            Random random = new Random();
+            int i = random.nextInt(100) + 1;
+            if (i <= 25) {
+                direction = "up";
+            }
+
+            else if (i > 25 && i <= 50){
+                direction = "down";
+            }
+
+            else if (i > 25 && i <= 50){
+                direction = "left";
+            }
+
+            else{
+                direction = "right";
+            }
+            actionLockC = 0;
+        }
+    }
+    
+
+    Color colour;
+    int actionLockC = 0;
+
+
+    
+
+
+
+    
+}
---
+++ JomAndTerry/src/main/java/main/AssetSetter.java
@@ -0,0 +1,20 @@
+package main;
+
+import entity.Enemy;
+
+import java.awt.*;
+
+public class AssetSetter {
+    
+    GamePanel gp;
+    public AssetSetter(GamePanel gp){
+        this.gp = gp;
+    }
+
+    public void setEnemy() {
+        gp.enemy[0] = new Enemy(gp, Color.BLUE);
+        gp.enemy[1] = new Enemy(gp, Color.RED);
+        gp.enemy[2] = new Enemy(gp, Color.ORANGE);
+    }
+    
+}
