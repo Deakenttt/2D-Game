33a754e3042681a10b5afa19627b557e65bf2e72
Deep Bhimani <dsb9@sfu.ca>
2023-02-27 21:35:47 -0800

    redesigned new map

--- JomAndTerry/src/main/java/tile/TileManager.java
+++ JomAndTerry/src/main/java/tile/TileManager.java
@@ -25,7 +25,7 @@ public class TileManager {
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow]; // mapTileNum will store all numbers in map.txt.
 
         getTileImage(); // Set each tiles bufferedImage.
-        loadMap("/assets/Map/map01.txt"); // Load a map text into 2D array and draw the map.
+        loadMap("/assets/Map/map02.txt"); // Load a map text into 2D array and draw the map.
     }
 
     public void getTileImage() {
@@ -40,11 +40,36 @@ public class TileManager {
             tile[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/wall.png")));
             tile[1].collision = true;
 
-            // Water
+            // outter wall
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/Tiles/old/water.png")));
+            tile[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/outer_wall.png")));
+            tile[2].collision = true;
 
+            // table
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/table.png")));
+            tile[3].collision = true;
 
+            // chair
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/chair.png")));
+            tile[4].collision = true;
+            
+            // candles
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/candles.png")));
+            tile[5].collision = true;
+
+            // oven
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/oven.png")));
+            tile[6].collision = true;
+
+            // plant
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/assets/new_tiles/plant.png")));
+            tile[7].collision = true;
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
