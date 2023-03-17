766c173d11516bc44bba0dcecbdaa350ad8269f7
kcl35 <kcl35@sfu.ca>
2023-03-17 11:41:00 -0700

    JavaDocs for entity

--- JomAndTerry/src/main/java/entity/Entity.java
+++ JomAndTerry/src/main/java/entity/Entity.java
@@ -6,33 +6,44 @@ import utility.UtilityTool;
 import javax.imageio.ImageIO;
 import java.util.Objects;
 import java.io.IOException;
-
 import java.awt.image.BufferedImage;
 
 // Blueprint for character classes
 import java.awt.*;
 
 /**
- * @Des This is an Entity class for Player, Cat
- */
+* Entity class representing the Player and Cat entities in the game.
+* It contains information about entity's coordinates, speed, direction, and animations.
+* It also has methods for updating the entity's position and drawing the entity on the game screen.
+*/
 public class Entity {
-
-    // ENTITY'S COORDINATE AND SPEED
     public int x, y;
     public int speed;
     GamePanel gp;
+    String name;
 
     // It describes an Image with an accessible buffer of image data.
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
     public String direction;
     public boolean doMove = true; // only move the entity when doMove is true.
 
-    // FOR ANIMATION
+    // For animation
     public int spriteCounter = 0;
     public int spriteNum = 1;
     public boolean onPath = false;
-    String name;
 
+    // For collision
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
+
+    // For saving the default x and y values
+    public int solidAreaDefaultX, solidAreaDefaultY;
+
+    /**
+    * Constructs an Entity object with default values.
+    *
+    * @param gp The game panel of the game.
+    */
     public Entity(GamePanel gp) {
         this.gp = gp;
         // SOLID AREA FOR COLLISION
@@ -44,13 +55,17 @@ public class Entity {
         getPlayerImage();
 
     }
-
+    /**
+     * Sets the default values of an entity.
+     */
     public void setDefaultValues() {
         x = 48;
         y = 48;
         onPath = true;  // Using the A* setAction on SmCat
-
     }
+    /**
+    * Sets the image of an entity.
+    */
     public void getPlayerImage() {
         up1 = setup("%s_up_1".formatted(name));
         up2 = setup("%s_up_2".formatted(name));
@@ -65,7 +80,11 @@ public class Entity {
         right2 = setup("%s_right_2".formatted(name));
 
     }
-
+    /**
+    * Loads and scales a BufferedImage from the "/assets" directory with the given imageName.
+    * @param imageName the name of the image file to load
+    * @return the scaled BufferedImage object
+    */
     public BufferedImage setup(String imageName) {
         UtilityTool utilityTool = new UtilityTool();
         BufferedImage image = null;
@@ -77,11 +96,15 @@ public class Entity {
         }
         return image;
     }
-
+    /**
+    * Sets the action of the entity.
+    */
     public void setAction() {
     }
     
-
+    /**
+    * Updates the position and sprite of the entity.
+    */
     public void update() {
         if (gp.player.doMove && !collisionOn) {
             switch (direction) {
@@ -109,15 +132,11 @@ public class Entity {
                 spriteNum = 1;
             spriteCounter = 0;
         }
-    }
-
-    // FOR COLLISION
-    public Rectangle solidArea;
-    public boolean collisionOn = false;
-
-    // SAVING THE DEFAULT VALUE OF X AND Y
-    public int solidAreaDefaultX, solidAreaDefaultY;
-
+    }            
+    /**
+    * Draws the sprite on the graphics context.
+    * @param g2 the graphics context to draw on
+    */
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
         switch (direction) {
